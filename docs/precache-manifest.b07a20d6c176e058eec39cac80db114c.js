c227c9d213ad6e6b0fe2daa8c9e201ab01686c80
Aia Rupsom <aiale88@gmail.com>
2019-09-27 17:34:18 +0200

    deploy new

---
+++ docs/precache-manifest.b07a20d6c176e058eec39cac80db114c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043f7e0c728412790120b41fe03a6e49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f89bae1ad0732f304848",
+    "url": "./static/css/main.5c376dd4.chunk.css"
+  },
+  {
+    "revision": "db9d1be80f81626e8c39",
+    "url": "./static/js/2.2405e8bd.chunk.js"
+  },
+  {
+    "revision": "f89bae1ad0732f304848",
+    "url": "./static/js/main.80c26ba1.chunk.js"
+  },
+  {
+    "revision": "3f51f1b5a76559e676b2",
+    "url": "./static/js/runtime~main.e97d81c8.js"
+  }
+]);
